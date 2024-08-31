2651a3fee24b2147b963c131d87d6c8c25bacb9f
fredegd <124848643+fredegd@users.noreply.github.com>
2024-08-31 13:13:49 +0200

    Customer Feedback: KKW title first slide

--- src/components/Cases/Kunstkraftwerk/index.jsx
+++ src/components/Cases/Kunstkraftwerk/index.jsx
@@ -8,26 +8,29 @@ function Kunstkraftwerk({ gallery }) {
     <ul className="    p-0 m-0 bg-black">
       <li
         id="gallery-item-1"
-        className="w-[100vw] md:w-[100vw] h-[120vh]  flex flex-col items-center justify-center text-white
+        className="w-[100vw] md:w-[100vw] h-[120vh]  flex flex-col items-center justify-center  text-white
         bg-[length:300%] md:bg-cover bg-[left_40%_top_-5rem]  md:bg-center bg-no-repeat sticky -top-[40%] md:-top-48"
         style={{
           backgroundImage: `url(${gallery[0].caseImg})`,
         }}
       >
         {/* desktop */}
-        <div className="hidden md:flex flex-col gap-8 md:text-[4.2vw]    w-10/12">
-          <h1 className="  tracking-normal   font-semibold  h-5 ">
+        {/* desktop */}
+        <div className="hidden md:flex flex-col      w-10/12">
+          <h1 className="  tracking-normal   font-semibold  md:text-[4.08vw] leading-tight">
             <ScrambleEffect tInput={gallery[0].title1} />
           </h1>
-          <h1 className="  tracking-normal font-semibold self-end ">
+        </div>
+        <div className="hidden md:flex  items-end  w-10/12 ">
+          <p className="   w-4/12 md:w-1/2 text-sm md:text-md lg:text-[1.12vw] font-medium">
+            <ScrambleEffect tInput={gallery[0].subtitle} />
+          </p>
+          <h1 className="  md:w-1/2  tracking-normal font-semibold md:text-[4.08vw] leading-5 text-right">
             <ScrambleEffect tInput={gallery[0].title2} />
           </h1>
         </div>
-        <div className="hidden md:flex  items-center  w-10/12  -mt-16">
-          <p className="   w-4/12 md:w-full text-sm md:text-md lg:text-[1.12vw] font-medium">
-            <ScrambleEffect tInput={gallery[0].subtitle} />
-          </p>
-        </div>
+
+        {/* mobile */}
         {/* mobile */}
         <div className="flex flex-col md:hidden items-start  justify-end w-10/12 h-full pb-60 md:pb-32">
           <h1 className=" tracking-normal text-[11vw] sm:text[11vw]  leading-none font-semibold my-4">
